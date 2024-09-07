9654611f4d6dad7286e45692223d2e8736152497
manuelDisacomp <manuel@disacomp.com>
2024-09-07 17:10:53 -0400

    sync

--- src/components/CheckoutSideMenu/CheckoutSideMenu.jsx
+++ src/components/CheckoutSideMenu/CheckoutSideMenu.jsx
@@ -21,14 +21,14 @@ const CheckoutSideMenu = () => {
             className={`${(isCheckoutSideMenuOpen) ? 'flex' : 'hidden'} pl-6 pr-6 product-detail flex-col fixed bg-white right-4 rounded-lg overflow-y-auto`}
         >
             <div className='flex justify-between items-center pt-4'>
-                <h2 className='font-medium text-xl'>Checkout</h2>
+                <h2 className='font-medium text-xl'>Carrito</h2>
                 <FaRegCircleXmark
                     className='cursor-pointer text-2xl'
                     onClick={closeCheckoutSideMenu}
                 />
             </div> 
 
-            {/* //TODO: hacer las cartas */}
+            
         </aside>
     )
 }
